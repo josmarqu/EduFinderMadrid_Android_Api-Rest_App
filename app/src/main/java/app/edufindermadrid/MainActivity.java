7e340c2db21d3c4b9a0bae6e0fb5f6c810b28dec
Jose Marquez <josemarquezmontoro2@gmail.com>
2023-03-02 15:46:34 +0100

    MainActivity funcionality implemented

--- app/src/main/java/app/edufindermadrid/MainActivity.java
+++ app/src/main/java/app/edufindermadrid/MainActivity.java
@@ -1,13 +1,26 @@
 package app.edufindermadrid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button btnShow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButton();
+    }
+
+    private void initButton() {
+        btnShow = (Button) findViewById(R.id.btnShow);
+        btnShow.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SchoolListActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
