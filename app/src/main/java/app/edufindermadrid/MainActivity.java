8e3a8675b0b894a5257f3f5905510de77129102f
Jose Marquez <josemarquezmontoro2@gmail.com>
2023-03-02 16:17:00 +0100

    project migrated to androidx libraries and added ActionBar to SchoolListActivity

--- app/src/main/java/app/edufindermadrid/MainActivity.java
+++ app/src/main/java/app/edufindermadrid/MainActivity.java
@@ -1,9 +1,8 @@
 package app.edufindermadrid;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
